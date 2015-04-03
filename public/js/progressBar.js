30be0a65d39ca592accbcf5b386d72b52d529a1c
GlennClaranet <glenn.inizan@fr.clara.net>
2015-04-03 16:20:38 +0200

    End of plugin progressBar with external function

--- public/js/progressBar.js
+++ public/js/progressBar.js
@@ -35,11 +35,11 @@
 		var initialize = function(){
 			
 			// On vide le conteneur
-			that.html('');
+			that.empty();
 				
 			
-			var test ={
-				"zabbix" : 66,
+			var values ={
+				"zabbix" : 5,
 				"wpm" : 46,
 				"oraconsole" : 69,
 				"nagios-sd" : 6,
@@ -47,63 +47,31 @@
 				"aws" : 8
 			}
 			
-			console.log(repartitionProportionnelle(100,test));
-			
-			var progressBarBadge=[];
-			progressBarBadge.push($('<div>',{"class":"legend-zabbix"}).append([
-				$('<span>',{"class":"label-legend"}).html("Zabbix"),
-				$('<span>',{"class":"badge badge-legend"}).html("32")
-			]));
-			progressBarBadge.push($('<div>',{"class":"legend-wpm"}).append([
-				$('<span>',{"class":"label-legend"}).html("WPM"),
-				$('<span>',{"class":"badge badge-legend"}).html("23")
-			]));
-			progressBarBadge.push($('<div>',{"class":"legend-oraconsole"}).append([
-				$('<span>',{"class":"label-legend"}).html("Oraconsole"),
-				$('<span>',{"class":"badge badge-legend"}).html("32")
-			]));
-			progressBarBadge.push($('<div>',{"class":"legend-nagio-sd"}).append([
-				$('<span>',{"class":"label-legend"}).html("Nagios-SD"),
-				$('<span>',{"class":"badge badge-legend"}).html("3")
-			]));
-			progressBarBadge.push($('<div>',{"class":"legend-typhon"}).append([
-				$('<span>',{"class":"label-legend"}).html("Typhon"),
-				$('<span>',{"class":"badge badge-legend"}).html("5")
-			]));
-			progressBarBadge.push($('<div>',{"class":"legend-aws"}).append([
-				$('<span>',{"class":"label-legend"}).html("AWS"),
-				$('<span>',{"class":"badge badge-legend"}).html("4")
-			]));
-			
-			var progressBar=[];
-			progressBar.push($('<div>',{"class":"progress-bar progress-bar-zabbix"}).css('width',"32%"));
-			progressBar.push($('<div>',{"class":"progress-bar progress-bar-wpm"}).css('width',"23%"));
-			progressBar.push($('<div>',{"class":"progress-bar progress-bar-oraconsole"}).css('width',"32%"));
-			progressBar.push($('<div>',{"class":"progress-bar progress-bar-nagios-sd"}).css('width',"3%"));
-			progressBar.push($('<div>',{"class":"progress-bar progress-bar-typhon"}).css('width',"5%"));
-			progressBar.push($('<div>',{"class":"progress-bar progress-bar-aws"}).css('width',"4%"));
-			
 			// Création du squelette
 			that.append(
 				$('<div>',{ "class" : "content"}).append([
 					$('<div>', { "class" : "row"}).append(
-						$('<div>',{"class" : "progress-legend"}).append(progressBarBadge)
+						$('<div>',{"class" : "progress-legend"})
 					),
 					$('<div>', { "class" : "row"}).append(
-						$('<div>', { 'class' : "progress" }).append(progressBar)
+						$('<div>', { 'class' : "progress" })
 					),
 				])
 			);
 			
+			update(values);
 			
 			return that;
 		};
 		
+		// Permet un repartition proportionnelle des votes en fonction du nombre de place
 		var repartitionProportionnelle=function(place, votes){
+			// Clone des votes
+			var resteVotes=jQuery.extend({}, votes);
 			var sumVotes=0;
 			var res={};
-			for(var label in votes){
-				sumVotes+=votes[label];
+			for(var label in resteVotes){
+				sumVotes+=resteVotes[label];
 				res[label]=0;
 			}
 			
@@ -115,17 +83,17 @@
 				var labelMax="";
 				var voteMax=0;
 				
-				for(var label in votes){
-					if(voteMax < votes[label]){
-						voteMax=votes[label];
+				for(var label in resteVotes){
+					if(voteMax < resteVotes[label]){
+						voteMax=resteVotes[label];
 						labelMax=label;
 					}
 				}
 				
 				if(labelMax!=""){
 					res[labelMax]+=1;
-					votes[labelMax]-=onePlace;
-					if(votes[labelMax]<=0) delete votes[labelMax];
+					resteVotes[labelMax]-=onePlace;
+					if(resteVotes[labelMax]<=0) delete resteVotes[labelMax];
 					
 					nbVotes++;
 				}else{
@@ -136,7 +104,26 @@
 			return res;
 		}
 		
-		var update = function(val){
+		var update = function(values){
+			
+			var repartition=repartitionProportionnelle(100,values);
+			
+			var progressBarBadge=[];
+			var progressBar=[];
+			
+			for(var label in repartition){
+				progressBarBadge.push($('<div>',{"class":"legend-"+label}).append([
+       				$('<span>',{"class":"label-legend"}).html(label),
+       				$('<span>',{"class":"badge badge-legend"}).html(values[label])
+       			]));
+				
+				progressBar.push($('<div>',{"class":"progress-bar progress-bar-"+label}).css('width',repartition[label]+"%"));
+			}
+			
+
+			that.find(".progress-legend").empty().append(progressBarBadge);
+			that.find(".progress").empty().append(progressBar);
+			
 			
 			return that;
 		};
